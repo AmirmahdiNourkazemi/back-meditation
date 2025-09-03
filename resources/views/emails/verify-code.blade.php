1b33b89ec221f95c0830844e02f3fb23413c30c0
Amirmahdi Nourkazemi <nourkazemi80@gmail.com>
2025-09-03 12:47:31 +0330

    fix: style of verify code

--- resources/views/emails/verify-code.blade.php
+++ resources/views/emails/verify-code.blade.php
@@ -2,8 +2,8 @@
 <html lang="fa" dir="rtl">
 <body style="text-align: right; font-family: Tahoma, Arial, sans-serif;">
     <h2>کد تأیید شما برای {{ config('app.name') }}</h2>
-    <p>برای تأیید ایمیل خود از کد زیر استفاده کنید:</p>
+    <p>:برای تأیید ایمیل خود از کد زیر استفاده کنید</p>
     <h1>{{ $code }}</h1>
-    <p>این کد تا ۱۵ دقیقه معتبر است.</p>
+    <p>.این کد تا ۱۵ دقیقه معتبر است</p>
 </body>
 </html>
